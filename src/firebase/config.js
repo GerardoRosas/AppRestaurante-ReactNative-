05dcfcd49df5729a588c6f276403c7cef9b1ca80
GerardoRosas <rosas.ruizlg@gmail.com>
2020-07-18 23:51:54 -0500

    formulario con Formik

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDfRN8R-xkR2lidVLm5cocGJCXrCosn5QU",
+    authDomain: "restaurante-90a1a.firebaseapp.com",
+    databaseURL: "https://restaurante-90a1a.firebaseio.com",
+    projectId: "restaurante-90a1a",
+    storageBucket: "restaurante-90a1a.appspot.com",
+    messagingSenderId: "631103019560",
+    appId: "1:631103019560:web:66238c9b5a9fae54f3731d",
+    measurementId: "G-NPHTMTRJZW"
+  };
+
+export default firebaseConfig;
